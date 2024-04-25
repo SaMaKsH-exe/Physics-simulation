bcb7e92791f35b421b0e395d9285dd9e0cdcedec
Samaksh <Samaksh13kaushik@gmail.com>
2024-04-25 13:36:40 +0200

    Updated routes

--- physics_simulation/src/app/page.tsx
+++ physics_simulation/src/app/page.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
   return (
     <div>
       <header className="header">
-        <div className="logo">Your Project Name</div>
+        <div className="logo">SK</div>
         <nav className="nav">
           <ul>
             <li><Link href="https://github.com/SaMaKsH-exe/Physics-simulation">Source Code</Link></li>
@@ -18,7 +18,7 @@ const Home: React.FC = () => {
       <div className="home-container">
         <h1 className="home-title">Physics Simulation</h1>
         <div className="button-container">
-          <Link href="/DoublePnedulum" passHref={true} legacyBehavior>
+          <Link href="/DoublePendulum" passHref={true} legacyBehavior>
             <a className="home-button">Double Pendulum</a>
           </Link>
           <Link href="/FluidSimulation" passHref={true} legacyBehavior>
